9fa0a6559ad0d2f9c4a30d7340e4c2b24e0dcd10
Douglas Patterson <douglaspatterson801@gmail.com>
2019-05-20 08:00:36 -0600

    initial commit

---
+++ codeChallenge3/codeChallenge3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  codeChallenge3
+//
+//  Created by Douglas Patterson on 5/20/19.
+//  Copyright © 2019 Douglas Patterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

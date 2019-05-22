95ff9b8ec3f0ee8d3a97ad8422d523ae0c4f9c52
Douglas Patterson <douglaspatterson801@gmail.com>
2019-05-22 09:31:10 -0600

    now makes successful api call. needs to define jsonObject in order to materialize search results.

---
+++ codeChallenge3/StoreItemController.swift
@@ -0,0 +1,40 @@
+//
+//  StoreItemController.swift
+//  codeChallenge3
+//
+//  Created by Douglas Patterson on 5/20/19.
+//  Copyright © 2019 Douglas Patterson. All rights reserved.
+//
+
+import UIKit
+
+struct StoreItemController {
+    
+    func fetchItems(matching query: [String : String], completion: @escaping ([StoreItem]?) -> Void) {
+        
+        
+        let apiKey = "apikey=ec7544d4"
+        let baseURL = URL(string: "http://www.omdbapi.com/?&\(apiKey)")!
+        
+        guard let url = baseURL.withQueries(query) else {
+            completion(nil)
+            print("Unable to build URL with supplied queries")
+            return
+        }
+     
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            let decoder = JSONDecoder()
+            if let data = data,
+                let storeItems = try? decoder.decode(StoreItems.self, from: data) {
+                completion(storeItems.results)
+            } else {
+                print("Either no data was returned, or data was not serialized.")
+                
+                completion(nil)
+                return
+            }
+        }
+        
+        task.resume()
+    }
+}
---
+++ codeChallenge3/codeChallenge3/Controller/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  codeChallenge3
+//
+//  Created by Douglas Patterson on 5/21/19.
+//  Copyright © 2019 Douglas Patterson. All rights reserved.
+//
+
+import Foundation
